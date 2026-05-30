a775ec4e115af8833125ef4e039406b1c7f8311c
noamsalomon50 <noamsalomon50@gmail.com>
2026-05-30 10:17:40 +0300

    Changed functions to be arrow functions, added notes that all number inputs are natural numbers, changed minor logic

--- Functions_1.js
+++ Functions_1.js
@@ -1,16 +1,17 @@
+//all number inputs in this excersize are natural numbers (positive integers!)
+//changed to arrow functions
+
 // ex 1 in functions 1
-function getLastDigit(number){
-    return Math.abs(number % 10);
-}
+const getLastDigit = number => number % 10;
 //console.log(getLastDigit(1235))
 
 
 // ex 2 in functions 1
-function getAllButLastDigit(number) {
+const getAllButLastDigit = number => {
     const strNum = String(number);
     if (strNum.length === 1){
-        return null
-    };
+        return null;
+    }
     return parseInt(strNum.slice(0,-1), 10);
 }
 //console.log(getAllButLastDigit(1))
@@ -18,20 +19,16 @@ function getAllButLastDigit(number) {
 
 
 // ex 3 in functions 1
-function getAmountOfDigits(number){
-    return String(number).length;
-}
+const getAmountOfDigits = number => String(number).length;
 //console.log(getAmountOfDigits(1235))
 
 
 //ex 4 in functions 1
-function getDigitInIndex(number, i){
-    const strNumRev = String(number).split("").reverse().join("");
-    if (i < strNumRev.length){
-        return parseInt(strNumRev[i], 10)  
-    };
-    return 0;
+const getDigitInIndex = (number, i) => {
+    const strNum = String(number)
+    if(i >= strNum.length){
+        return 0;
+    }
+    return parseInt(strNum[strNum.length - 1 - i], 10);
 }
-
-//console.log(getDigitInIndex(1235,3))
-
+//console.log(getDigitInIndex(1235,0))
